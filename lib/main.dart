aa53c6a53105562dd722820f3cb82b60f9d8acaf
Camilo Cespedes <cacespedes@uninorte.edu.co>
2023-10-05 15:19:30 -0500

    Bloc-like main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:bookpal/app/pages/main_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 
 void main() {
-  runApp(const BookPal());
+  runApp(const BookPalState());
+}
+
+class BookPalState extends StatelessWidget {
+  const BookPalState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [],
+      child: BookPal(),
+    );
+  }
 }
 
 
