b51118362336a78c36dd37a3a0a598b2d07708d4
Camilo Cespedes <cacespedes@uninorte.edu.co>
2023-10-12 08:59:37 -0500

    Implemented Company Rep methods

--- lib/data/repositories/company_rep_implementation.dart
+++ lib/data/repositories/company_rep_implementation.dart
@@ -1,12 +1,31 @@
 
 
+import 'package:bookpal/data/data_sources/remote/general_database_api_service.dart';
+import 'package:bookpal/data/helpers/response_verifier.dart';
 import 'package:bookpal/domain/entities/company.dart';
 import 'package:bookpal/domain/repositories/company_repository.dart';
 import 'package:bookpal/domain/resources/data_state.dart';
+import 'package:dio/dio.dart';
 
 class CompanyRepositoryImplementation implements CompanyRepository {
+
+  final GeneralDatabaseApiService _generalDatabaseApiService;
+
+  CompanyRepositoryImplementation(this._generalDatabaseApiService);
+
   @override
-  Future<DataState<Company>> getCompany() {
-    throw UnimplementedError();
+  Future<DataState<Company>> getCompany(String id) async {
+    try {
+      final httpResponse = await _generalDatabaseApiService.getCompany(
+        id: id,
+      );
+
+      final ResponseVerifier<Company> responseVerifier = ResponseVerifier<Company>();
+
+      return responseVerifier.validateResponse(httpResponse);
+      
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
   }
 }
--- lib/domain/repositories/company_repository.dart
+++ lib/domain/repositories/company_repository.dart
@@ -4,5 +4,5 @@ import 'package:bookpal/domain/entities/company.dart';
 import 'package:bookpal/domain/resources/data_state.dart';
 
 abstract class CompanyRepository {
-  Future<DataState<Company>> getCompany();
+  Future<DataState<Company>> getCompany(String id);
 }
