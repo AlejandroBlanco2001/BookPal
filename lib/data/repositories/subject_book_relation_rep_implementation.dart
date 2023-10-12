c1d4c7d0dab6a9058b661bf1119f02639e72d09c
Camilo Cespedes <cacespedes@uninorte.edu.co>
2023-10-12 09:00:44 -0500

    Implemented Subject-Book Rep methods

--- lib/data/repositories/subject_book_relation_rep_implementation.dart
+++ lib/data/repositories/subject_book_relation_rep_implementation.dart
@@ -1,23 +1,54 @@
 
 
+import 'package:bookpal/data/data_sources/remote/general_database_api_service.dart';
+import 'package:bookpal/data/helpers/response_verifier.dart';
 import 'package:bookpal/data/models/subject_book_relation_model.dart';
 import 'package:bookpal/domain/repositories/subject_book_relation_repository.dart';
 import 'package:bookpal/domain/resources/data_state.dart';
+import 'package:dio/dio.dart';
 
 class SubjectBookRelationRepositoryImplementation implements SubjectBookRelationRepository {
+
+  final GeneralDatabaseApiService _generalDatabaseApiService;
+
+  SubjectBookRelationRepositoryImplementation(this._generalDatabaseApiService);
   
   @override
-  Future<DataState<SubjectBookRelationModel>> getRelation() {
-    throw UnimplementedError();
+  Future<DataState<SubjectBookRelationModel>> getRelation(String id) async {
+    try {
+      final httpResponse = await _generalDatabaseApiService.getRelation(
+        id: id,
+      );
+      final ResponseVerifier<SubjectBookRelationModel> responseVerifier = ResponseVerifier<SubjectBookRelationModel>();
+      return responseVerifier.validateResponse(httpResponse);
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
   }
   
   @override
-  Future<DataState<List<String>>> getRelatedSubjects() {
-    throw UnimplementedError();
+  Future<DataState<List<String>>> getRelatedSubjects(String bookId) async {
+    try {
+      final httpResponse = await _generalDatabaseApiService.getRelatedSubjects(
+        bookId: bookId,
+      );
+      final ResponseVerifier<List<String>> responseVerifier = ResponseVerifier<List<String>>();
+      return responseVerifier.validateResponse(httpResponse);
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
   }
 
   @override
-  Future<DataState<List<String>>> getRelatedBooks() {
-    throw UnimplementedError();
+  Future<DataState<List<String>>> getRelatedBooks(String subjectId) async {
+    try {
+      final httpResponse = await _generalDatabaseApiService.getRelatedBooks(
+        subjectId: subjectId,
+      );
+      final ResponseVerifier<List<String>> responseVerifier = ResponseVerifier<List<String>>();
+      return responseVerifier.validateResponse(httpResponse);
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
   }
 }
--- lib/domain/repositories/subject_book_relation_repository.dart
+++ lib/domain/repositories/subject_book_relation_repository.dart
@@ -4,7 +4,7 @@ import 'package:bookpal/domain/entities/subject_book_relation.dart';
 import 'package:bookpal/domain/resources/data_state.dart';
 
 abstract class SubjectBookRelationRepository {
-  Future<DataState<SubjectBookRelation>> getRelation();
-  Future<DataState<List<String>>> getRelatedSubjects();
-  Future<DataState<List<String>>> getRelatedBooks();
+  Future<DataState<SubjectBookRelation>> getRelation(String id);
+  Future<DataState<List<String>>> getRelatedSubjects(String bookId);
+  Future<DataState<List<String>>> getRelatedBooks(String subjectId);
 }
