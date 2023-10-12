cb6ce4cd90029233de743c57922f5c20915052ca
Camilo Cespedes <cacespedes@uninorte.edu.co>
2023-10-12 09:01:10 -0500

    Implemented User Rep methods

--- lib/data/repositories/user_rep_implementation.dart
+++ lib/data/repositories/user_rep_implementation.dart
@@ -1,10 +1,30 @@
+
+import 'package:bookpal/data/data_sources/remote/general_database_api_service.dart';
+import 'package:bookpal/data/helpers/response_verifier.dart';
 import 'package:bookpal/data/models/user_model.dart';
 import 'package:bookpal/domain/repositories/user_repository.dart';
 import 'package:bookpal/domain/resources/data_state.dart';
+import 'package:dio/dio.dart';
 
 class UserRepositoryImplementation implements UserRepository {
+  final GeneralDatabaseApiService _generalDatabaseApiService;
+
+  UserRepositoryImplementation(this._generalDatabaseApiService);
+
   @override
-  Future<DataState<UserModel>> getUser() {
-    throw UnimplementedError();
+  Future<DataState<UserModel>> getUser(String id) async {
+    try {
+      final httpResponse = await _generalDatabaseApiService.getUser(
+        id: id,
+      );
+
+      final ResponseVerifier<UserModel> responseVerifier =
+          ResponseVerifier<UserModel>();
+
+      return responseVerifier.validateResponse(httpResponse);
+      
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
   }
 }
--- lib/domain/repositories/user_repository.dart
+++ lib/domain/repositories/user_repository.dart
@@ -2,5 +2,5 @@ import 'package:bookpal/domain/entities/user.dart';
 import 'package:bookpal/domain/resources/data_state.dart';
 
 abstract class UserRepository {
-  Future<DataState<User>> getUser();
+  Future<DataState<User>> getUser(String id);
 }
