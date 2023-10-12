e2e8b3b041ff9158349972a73773903e29d42852
Camilo Cespedes <cacespedes@uninorte.edu.co>
2023-10-12 08:59:45 -0500

    Implemented Fine Rep methods

--- lib/data/repositories/fine_rep_implementation.dart
+++ lib/data/repositories/fine_rep_implementation.dart
@@ -1,17 +1,41 @@
 
 
+import 'package:bookpal/data/data_sources/remote/general_database_api_service.dart';
+import 'package:bookpal/data/helpers/response_verifier.dart';
 import 'package:bookpal/domain/entities/fine.dart';
 import 'package:bookpal/domain/repositories/fine_repository.dart';
 import 'package:bookpal/domain/resources/data_state.dart';
+import 'package:dio/dio.dart';
 
 class FineRepositoryImplementation implements FineRepository {
+
+  final GeneralDatabaseApiService _generalDatabaseApiService;
+
+  FineRepositoryImplementation(this._generalDatabaseApiService);
+
   @override
-  Future<DataState<Fine>> getFine() {
-    throw UnimplementedError();
+  Future<DataState<Fine>> getFine(String id) async {
+    try {
+      final httpResponse = await _generalDatabaseApiService.getFine(
+        id: id,
+      );
+      final ResponseVerifier<Fine> responseVerifier = ResponseVerifier<Fine>();
+      return responseVerifier.validateResponse(httpResponse);
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
   }
 
   @override
-  Future<DataState<List<Fine>>> getFinesByUser() {
-    throw UnimplementedError();
+  Future<DataState<List<Fine>>> getFinesByUser(String userId) async {
+    try {
+      final httpResponse = await _generalDatabaseApiService.getFinesByUser(
+        userId: userId,
+      );
+      final ResponseVerifier<List<Fine>> responseVerifier = ResponseVerifier<List<Fine>>();
+      return responseVerifier.validateResponse(httpResponse);
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
   }
 }
--- lib/domain/repositories/fine_repository.dart
+++ lib/domain/repositories/fine_repository.dart
@@ -4,6 +4,6 @@ import 'package:bookpal/domain/entities/fine.dart';
 import 'package:bookpal/domain/resources/data_state.dart';
 
 abstract class FineRepository {
-  Future<DataState<Fine>> getFine();
-  Future<DataState<List<Fine>>> getFinesByUser();
+  Future<DataState<Fine>> getFine(String id);
+  Future<DataState<List<Fine>>> getFinesByUser(String userId);
 }
