85648ec485c331c7ae553c04888fc3a97e98e5d8
Camilo Cespedes <cacespedes@uninorte.edu.co>
2023-10-12 09:00:57 -0500

    Implemented Subject Rep methods

--- lib/data/repositories/subject_rep_implementation.dart
+++ lib/data/repositories/subject_rep_implementation.dart
@@ -1,12 +1,29 @@
 
 
+import 'package:bookpal/data/data_sources/remote/general_database_api_service.dart';
+import 'package:bookpal/data/helpers/response_verifier.dart';
+import 'package:bookpal/data/models/subject_model.dart';
 import 'package:bookpal/domain/entities/subject.dart';
 import 'package:bookpal/domain/repositories/subject_repository.dart';
 import 'package:bookpal/domain/resources/data_state.dart';
+import 'package:dio/dio.dart';
 
 class SubjectRepositoryImplementation implements SubjectRepository {
+
+  final GeneralDatabaseApiService _generalDatabaseApiService;
+
+  SubjectRepositoryImplementation(this._generalDatabaseApiService);
+
   @override
-  Future<DataState<Subject>> getSubject() {
-    throw UnimplementedError();
+  Future<DataState<Subject>> getSubject(String id) async {
+    try {
+      final httpResponse = await _generalDatabaseApiService.getSubject(
+        id: id,
+      );
+      final ResponseVerifier<SubjectModel> responseVerifier = ResponseVerifier<SubjectModel>();
+      return responseVerifier.validateResponse(httpResponse);
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
   }
 }
--- lib/domain/repositories/subject_repository.dart
+++ lib/domain/repositories/subject_repository.dart
@@ -4,5 +4,5 @@ import 'package:bookpal/domain/entities/subject.dart';
 import 'package:bookpal/domain/resources/data_state.dart';
 
 abstract class SubjectRepository {
-  Future<DataState<Subject>> getSubject();
+  Future<DataState<Subject>> getSubject(String id);
 }
