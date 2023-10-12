95ede263b876aba565e0393f13d0d4541a6a87c0
Camilo Cespedes <cacespedes@uninorte.edu.co>
2023-10-12 09:00:13 -0500

    Implemented Notification Rep methods

--- lib/data/repositories/notification_rep_implementation.dart
+++ lib/data/repositories/notification_rep_implementation.dart
@@ -1,16 +1,40 @@
 
 
+import 'package:bookpal/data/data_sources/remote/general_database_api_service.dart';
+import 'package:bookpal/data/helpers/response_verifier.dart';
 import 'package:bookpal/data/models/notification_model.dart';
 import 'package:bookpal/domain/repositories/notification_repository.dart';
 import 'package:bookpal/domain/resources/data_state.dart';
+import 'package:dio/dio.dart';
 
 class NotificationRepositoryImplementation implements NotificationRepository {
+
+  final GeneralDatabaseApiService _generalDatabaseApiService;
+
+  NotificationRepositoryImplementation(this._generalDatabaseApiService);
+
   @override
-  Future<DataState<NotificationModel>> getNotification() {
-    throw UnimplementedError();
+  Future<DataState<NotificationModel>> getNotification(String id) async {
+    try {
+      final httpResponse = await _generalDatabaseApiService.getNotification(
+        id: id,
+      );
+      final ResponseVerifier<NotificationModel> responseVerifier = ResponseVerifier<NotificationModel>();
+      return responseVerifier.validateResponse(httpResponse);
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
   }
   @override
-  Future<DataState<List<NotificationModel>>> getUserNotifications() {
-    throw UnimplementedError();
+  Future<DataState<List<NotificationModel>>> getUserNotifications(String userId) async {
+    try {
+      final httpResponse = await _generalDatabaseApiService.getUserNotifications(
+        userId: userId,
+      );
+      final ResponseVerifier<List<NotificationModel>> responseVerifier = ResponseVerifier<List<NotificationModel>>();
+      return responseVerifier.validateResponse(httpResponse);
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
   }
 }
--- lib/domain/repositories/notification_repository.dart
+++ lib/domain/repositories/notification_repository.dart
@@ -4,6 +4,6 @@ import 'package:bookpal/domain/entities/notification.dart';
 import 'package:bookpal/domain/resources/data_state.dart';
 
 abstract class NotificationRepository {
-  Future<DataState<Notification>> getNotification();
-  Future<DataState<List<Notification>>> getUserNotifications();
+  Future<DataState<Notification>> getNotification(String id);
+  Future<DataState<List<Notification>>> getUserNotifications(String userId);
 }
