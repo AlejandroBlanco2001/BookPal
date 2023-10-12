81c8314b5f4a70be1c359dad35b4dd5ddeaea31a
Camilo Cespedes <cacespedes@uninorte.edu.co>
2023-10-12 08:59:55 -0500

    Implemented Inventory Rep methods

--- lib/data/repositories/inventory_rep_implementation.dart
+++ lib/data/repositories/inventory_rep_implementation.dart
@@ -1,17 +1,41 @@
 
 
+import 'package:bookpal/data/data_sources/remote/general_database_api_service.dart';
+import 'package:bookpal/data/helpers/response_verifier.dart';
 import 'package:bookpal/data/models/inventory_model.dart';
 import 'package:bookpal/domain/repositories/inventory_repository.dart';
 import 'package:bookpal/domain/resources/data_state.dart';
+import 'package:dio/dio.dart';
 
 class InventoryRepositoryImplementation implements InventoryRepository {
+
+  final GeneralDatabaseApiService _generalDatabaseApiService;
+
+  InventoryRepositoryImplementation(this._generalDatabaseApiService);
+
   @override
-  Future<DataState<InventoryModel>> getInventory() {
-    throw UnimplementedError();
+  Future<DataState<InventoryModel>> getInventory(String id) async {
+    try {
+      final httpResponse = await _generalDatabaseApiService.getInventory(
+        id: id,
+      );
+      final ResponseVerifier<InventoryModel> responseVerifier = ResponseVerifier<InventoryModel>();
+      return responseVerifier.validateResponse(httpResponse);
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
   }
 
   @override
-  Future<DataState<InventoryModel>> getInventoryForItem() {
-    throw UnimplementedError();
+  Future<DataState<InventoryModel>> getInventoryForItem(String itemId) async {
+    try {
+      final httpResponse = await _generalDatabaseApiService.getInventoryForItem(
+        itemId: itemId,
+      );
+      final ResponseVerifier<InventoryModel> responseVerifier = ResponseVerifier<InventoryModel>();
+      return responseVerifier.validateResponse(httpResponse);
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
   }
 }
--- lib/domain/repositories/inventory_repository.dart
+++ lib/domain/repositories/inventory_repository.dart
@@ -4,6 +4,6 @@ import 'package:bookpal/domain/entities/inventory.dart';
 import 'package:bookpal/domain/resources/data_state.dart';
 
 abstract class InventoryRepository {
-  Future<DataState<Inventory>> getInventory();
-  Future<DataState<Inventory>> getInventoryForItem();
+  Future<DataState<Inventory>> getInventory(String id);
+  Future<DataState<Inventory>> getInventoryForItem(String itemId);
 }
