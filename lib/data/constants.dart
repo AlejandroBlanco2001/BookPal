ddd7ee97134ff590f5b39cc32271d3ea5a88edde
Camilo Cespedes <cacespedes@uninorte.edu.co>
2023-10-12 08:58:05 -0500

    Added logger and api_key to constants.dart

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -1,4 +1,8 @@
+import 'package:logger/logger.dart';
+
 const String baseUrl = 'https://nook-pal-api.onrender.com';
 const String contentType = 'application/json';
 
 const String apiKey = 'api_key';
+
+Logger logger = Logger();
