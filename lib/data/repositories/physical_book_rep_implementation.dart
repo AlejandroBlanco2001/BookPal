e6610c2c111f49fd6470f2f69eeac5e5e6c67435
Camilo Cespedes <cacespedes@uninorte.edu.co>
2023-10-12 09:00:24 -0500

    Implemented Physical Book Rep methods

--- lib/data/repositories/physical_book_rep_implementation.dart
+++ lib/data/repositories/physical_book_rep_implementation.dart
@@ -1,12 +1,28 @@
 
 
+import 'package:bookpal/data/data_sources/remote/general_database_api_service.dart';
+import 'package:bookpal/data/helpers/response_verifier.dart';
 import 'package:bookpal/data/models/physical_book_model.dart';
 import 'package:bookpal/domain/repositories/physical_book_repository.dart';
 import 'package:bookpal/domain/resources/data_state.dart';
+import 'package:dio/dio.dart';
 
 class PhysicalBookRepositoryImplementation implements PhysicalBookRepository {
+
+  final GeneralDatabaseApiService _generalDatabaseApiService;
+
+  PhysicalBookRepositoryImplementation(this._generalDatabaseApiService);
+
   @override
-  Future<DataState<PhysicalBookModel>> getPhysicalBook() {
-    throw UnimplementedError();
+  Future<DataState<PhysicalBookModel>> getPhysicalBook(String id) async {
+    try {
+      final httpResponse = await _generalDatabaseApiService.getPhysicalBook(
+        id: id,
+      );
+      final ResponseVerifier<PhysicalBookModel> responseVerifier = ResponseVerifier<PhysicalBookModel>();
+      return responseVerifier.validateResponse(httpResponse);
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
   }
 }
--- lib/domain/repositories/physical_book_repository.dart
+++ lib/domain/repositories/physical_book_repository.dart
@@ -4,5 +4,5 @@ import 'package:bookpal/domain/entities/physical_book.dart';
 import 'package:bookpal/domain/resources/data_state.dart';
 
 abstract class PhysicalBookRepository {
-  Future<DataState<PhysicalBook>> getPhysicalBook();
+  Future<DataState<PhysicalBook>> getPhysicalBook(String id);
 }
