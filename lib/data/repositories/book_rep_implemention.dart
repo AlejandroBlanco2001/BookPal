db939a2180d268e0d7a6b10b9a73ba2e33d31414
Camilo Cespedes <cacespedes@uninorte.edu.co>
2023-10-12 08:59:28 -0500

    Implemented Book Rep methods

--- lib/data/repositories/book_rep_implemention.dart
+++ lib/data/repositories/book_rep_implemention.dart
@@ -1,11 +1,30 @@
 
+import 'package:bookpal/data/data_sources/remote/general_database_api_service.dart';
+import 'package:bookpal/data/helpers/response_verifier.dart';
 import 'package:bookpal/data/models/book_model.dart';
 import 'package:bookpal/domain/repositories/book_repository.dart';
 import 'package:bookpal/domain/resources/data_state.dart';
+import 'package:dio/dio.dart';
 
 class BookRepositoryImplementation implements BookRepository {
+
+  final GeneralDatabaseApiService _generalDatabaseApiService;
+
+  BookRepositoryImplementation(this._generalDatabaseApiService);
+
   @override
-  Future<DataState<BookModel>> getBook(){
-    throw UnimplementedError();
+  Future<DataState<BookModel>> getBook(String id) async{
+    try {
+      final httpResponse = await _generalDatabaseApiService.getBook(
+        id: id,
+      );
+
+      final ResponseVerifier<BookModel> responseVerifier = ResponseVerifier<BookModel>();
+
+      return responseVerifier.validateResponse(httpResponse);
+      
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
   }
 }
--- lib/domain/repositories/book_repository.dart
+++ lib/domain/repositories/book_repository.dart
@@ -4,5 +4,5 @@ import 'package:bookpal/domain/entities/book.dart';
 import 'package:bookpal/domain/resources/data_state.dart';
 
 abstract class BookRepository {
-  Future<DataState<Book>> getBook();
+  Future<DataState<Book>> getBook(String id);
 }
