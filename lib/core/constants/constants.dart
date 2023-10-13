470382d184a5e06a3d388a3fd44196cb348046d7
Camilo Cespedes <cacespedes@uninorte.edu.co>
2023-10-13 10:01:50 -0500

    Eliminated apikey from constants

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -3,6 +3,4 @@ import 'package:logger/logger.dart';
 const String baseUrl = 'https://nook-pal-api.onrender.com';
 const String contentType = 'application/json';
 
-const String apiKey = 'api_key';
-
 Logger logger = Logger();
