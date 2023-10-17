447c03eec015b2531f29ea57fbdd39d463bc63cc
Camilo Cespedes <cacespedes@uninorte.edu.co>
2023-10-17 08:19:02 -0500

    Changed to stateless

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,30 @@
 import 'package:bookpal/app/pages/main_page/main_page.dart';
+import 'package:bookpal/core/injection_container.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
-void main() {
+Future<void> main() async {
+  await InitializeDependencies();
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences.getInstance().then((prefs) {
-    runApp(BookPalState(prefs:prefs));
+    runApp(BookPal(prefs: prefs));
   });
 }
 
-class BookPalState extends StatelessWidget {
-  const BookPalState({super.key, required this.prefs});
+class BookPal extends StatelessWidget {
+  const BookPal({super.key, required this.prefs});
 
   final SharedPreferences prefs;
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: const [],
-      child: const BookPal(),
-    );
-  }
-}
-
-
-class BookPal extends StatelessWidget {
-  const BookPal({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const BookPalHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const BookPalHomePage(title: 'Flutter Demo Home Page'),
+      );
   }
 }
