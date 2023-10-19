19bc140221ee894d040cc88c6ab1660432c8a148
Camilo Cespedes <cacespedes@uninorte.edu.co>
2023-10-19 07:14:53 -0500

    Remote user bloc included in view

--- lib/app/pages/main_page/main_page.dart
+++ lib/app/pages/main_page/main_page.dart
@@ -1,5 +1,7 @@
-
+import 'package:bookpal/presentation/user/remote_bloc/remote_user_bloc.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BookPalHomePage extends StatelessWidget {
   const BookPalHomePage({super.key, required this.title});
@@ -18,21 +20,41 @@ class BookPalHomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'User email:',
             ),
-            Text(
-              'User',
-              style: Theme.of(context).textTheme.headlineMedium,
+            BlocBuilder<RemoteUserBloc, RemoteUserState>(
+              builder: (_, state) {
+                if (state is RemoteUserLoading){
+                  return const CupertinoActivityIndicator();
+                }
+                if (state is RemoteUserInitial){
+                  return Text(
+                    state.id!,
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                }
+                if (state is RemoteUserError){
+                  return const Center(child: Icon(Icons.refresh));
+                }
+                if (state is RemoteUserLoaded){
+                  return Text(
+                    state.user!.email,
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                }
+                return const SizedBox();
+              },
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {},
+        onPressed: () => {
+          context.read<RemoteUserBloc>().add(const GetUser('5'))
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,34 @@
 import 'package:bookpal/app/pages/main_page/main_page.dart';
 import 'package:bookpal/core/injection_container.dart';
+import 'package:bookpal/presentation/user/remote_bloc/remote_user_bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> main() async {
   await InitializeDependencies();
   WidgetsFlutterBinding.ensureInitialized();
-  SharedPreferences.getInstance().then((prefs) {
-    runApp(BookPal(prefs: prefs));
-  });
+  runApp(const BookPal());
 }
 
 class BookPal extends StatelessWidget {
-  const BookPal({super.key, required this.prefs});
-
-  final SharedPreferences prefs;
+  const BookPal({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<RemoteUserBloc>(
+          create: (context) => sl()..add(InitUser()),
+        ),
+      ],
+      child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
         home: const BookPalHomePage(title: 'Flutter Demo Home Page'),
-      );
+      ),
+    );
   }
 }
