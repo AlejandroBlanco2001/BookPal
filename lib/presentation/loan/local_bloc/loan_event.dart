903b4d664f537736c6ded83ffc1ad8b376d6424e
Camilo Cespedes <cacespedes@uninorte.edu.co>
2023-10-22 13:54:21 -0500

    Loan basic blocs

---
+++ lib/presentation/loan/local_bloc/loan_event.dart
@@ -0,0 +1,8 @@
+part of 'loan_bloc.dart';
+
+sealed class LoanEvent extends Equatable {
+  const LoanEvent();
+
+  @override
+  List<Object> get props => [];
+}
---
+++ lib/presentation/loan/remote_bloc/remote_loan_state.dart
@@ -0,0 +1,55 @@
+part of 'remote_loan_bloc.dart';
+
+sealed class RemoteLoanState extends Equatable {
+
+  final LoanModel? loan;
+  final List<LoanModel>? loans;
+  final DioException? dioError;
+  final dynamic genericError;
+  final int? statusCode;
+
+  const RemoteLoanState({this.loan, this.loans, this.dioError, this.genericError, this.statusCode = 200});
+
+  @override
+  List<Object?> get props => [loan, loans, dioError, genericError, statusCode];
+}
+
+final class RemoteLoanInitial extends RemoteLoanState {}
+
+final class RemoteLoanLoading extends RemoteLoanState {}
+
+final class RemoteLoanLoaded extends RemoteLoanState {
+  const RemoteLoanLoaded(int statusCode, LoanModel loan) : super(statusCode: statusCode, loan: loan);
+
+  @override
+  List<Object?> get props => [statusCode, loan];
+}
+
+final class RemoteUserLoansLoaded extends RemoteLoanState {
+  const RemoteUserLoansLoaded(int statusCode, List<LoanModel> loans) : super(statusCode: statusCode, loans: loans);
+
+  @override
+  List<Object?> get props => [statusCode, loans];
+}
+
+final class RemoteLoanReturned extends RemoteLoanState {
+  const RemoteLoanReturned(int statusCode, LoanModel loan) : super(statusCode: statusCode, loan: loan);
+
+  @override
+  List<Object?> get props => [statusCode, loan];
+}
+
+final class RemoteLoanCreated extends RemoteLoanState {
+  const RemoteLoanCreated(int statusCode, LoanModel loan) : super(statusCode: statusCode, loan: loan);
+
+  @override
+  List<Object?> get props => [statusCode, loan];
+}
+
+final class RemoteLoanError extends RemoteLoanState {
+  const RemoteLoanError(DioException dioError, [int? statusCode]) : super(statusCode: statusCode, dioError: dioError);
+  const RemoteLoanError.genericError(dynamic genericError) : super(genericError: genericError);
+  
+  @override
+  List<Object?> get props => [statusCode, dioError];
+}
