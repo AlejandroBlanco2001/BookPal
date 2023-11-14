4b7a33d9f198cbb0b5055a45d6d2e48ac23b2be8
Camilo Cespedes <cacespedes@uninorte.edu.co>
2023-11-14 08:09:27 -0500

    Clickable rows

--- lib/app/widgets/home_page/book_row.dart
+++ lib/app/widgets/home_page/book_row.dart
@@ -1,22 +1,41 @@
+import 'package:bookpal/app/pages/book_description/book_description_page.dart';
 import 'package:bookpal/app/widgets/home_page/book_item.dart';
 import 'package:flutter/material.dart';
 
 class BookRow extends StatelessWidget {
+  const BookRow(
+      {Key? key,
+      required this.coverUrl,
+      required this.title,
+      required this.author})
+      : super(key: key);
 
-  const BookRow({Key? key, required this.coverUrl, required this.title, required this.author}) : super(key: key);
-
-  final String coverUrl ;
+  final String coverUrl;
   final String title;
   final String author;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.fromLTRB(24.0, 12.0, 0, 0),
-      child: BookItem(
-        imageUrl: coverUrl,
-        title: title,
-        author: author,
+    return Material(
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const BookDescription(),
+            ),
+          );
+        },
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(24.0, 6.0, 0, 6),
+          child: Ink(
+            child: BookItem(
+              imageUrl: coverUrl,
+              title: title,
+              author: author,
+            ),
+          ),
+        ),
       ),
     );
   }
