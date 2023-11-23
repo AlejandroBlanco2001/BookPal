954f5894ea67599a60023c01b173ba8102fbb8ae
Camilo Cespedes <cacespedes@uninorte.edu.co>
2023-11-23 18:11:27 -0500

    Navigate to profile/login page with home PP

--- lib/app/widgets/home_page/user_section.dart
+++ lib/app/widgets/home_page/user_section.dart
@@ -2,6 +2,8 @@ import 'package:bookpal/app/widgets/loading/basic_shimmer.dart';
 import 'package:bookpal/app/widgets/profile_page/default_circle_avatar.dart';
 import 'package:bookpal/core/util/utilities.dart';
 import 'package:bookpal/presentation/authentication/bloc/login_bloc.dart';
+import 'package:bookpal/presentation/navigation/bloc/navigation_bloc.dart';
+import 'package:bookpal/presentation/navigation/bloc/navigation_pages_bloc.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -23,72 +25,77 @@ class UserSection extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  BlocBuilder<LoginBloc, LoginState>(
-                    builder: (context, state) {
-                      if (state is LoginInitial) {
-                        return const DefaultCircleAvatar();
-                      } else if (state is LoginSuccess) {
-                        return FutureBuilder(
-                          future: Utilities.getProfileImageDownloadUrl(),
-                          builder: (context, snapshot) {
-                            if (snapshot.hasError) {
-                              return const DefaultCircleAvatar();
-                            } else if (snapshot.connectionState ==
-                                ConnectionState.waiting) {
-                              return ClipOval(
-                                child: Container(
-                                  width: 70,
-                                  height: 70,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(50),
-                                  ),
-                                  child: const ThemeShimmer(),
-                                ),
-                              );
-                            }
-                            return CachedNetworkImage(
-                              imageUrl: snapshot.data!,
-                              imageBuilder: (context, imageProvider) {
-                                return Container(
-                                  width: 70,
-                                  height: 70,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(50),
-                                    border: Border.all(
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .onPrimary,
-                                      width: 2,
+                  GestureDetector(
+                    onTap: () => context.read<NavigationBloc>().add(SwapPage(
+                        context.read<NavigationPagesBloc>().state.pages.length -
+                            1),),
+                    child: BlocBuilder<LoginBloc, LoginState>(
+                      builder: (context, state) {
+                        if (state is LoginInitial) {
+                          return const DefaultCircleAvatar();
+                        } else if (state is LoginSuccess) {
+                          return FutureBuilder(
+                            future: Utilities.getProfileImageDownloadUrl(),
+                            builder: (context, snapshot) {
+                              if (snapshot.hasError) {
+                                return const DefaultCircleAvatar();
+                              } else if (snapshot.connectionState ==
+                                  ConnectionState.waiting) {
+                                return ClipOval(
+                                  child: Container(
+                                    width: 70,
+                                    height: 70,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(50),
                                     ),
-                                  ),
-                                  child: CircleAvatar(
-                                    radius: 35,
-                                    backgroundColor: Colors.white,
-                                    backgroundImage: imageProvider,
+                                    child: const ThemeShimmer(),
                                   ),
                                 );
-                              },
-                              placeholder: (context, url) {
-                                return const ThemeShimmer();
-                              },
-                              fit: BoxFit.cover,
-                            );
-                          },
-                        );
-                      } else if (state is LoggingOut) {
-                        return const DefaultCircleAvatar();
-                      } else if (state is LoginLoading) {
-                        return const ClipOval(
-                          child: SizedBox(
-                            width: 70,
-                            height: 70,
-                            child: ThemeShimmer(),
-                          ),
-                        );
-                      } else {
-                        return const DefaultCircleAvatar();
-                      }
-                    },
+                              }
+                              return CachedNetworkImage(
+                                imageUrl: snapshot.data!,
+                                imageBuilder: (context, imageProvider) {
+                                  return Container(
+                                    width: 70,
+                                    height: 70,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(50),
+                                      border: Border.all(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .onPrimary,
+                                        width: 2,
+                                      ),
+                                    ),
+                                    child: CircleAvatar(
+                                      radius: 35,
+                                      backgroundColor: Colors.white,
+                                      backgroundImage: imageProvider,
+                                    ),
+                                  );
+                                },
+                                placeholder: (context, url) {
+                                  return const ThemeShimmer();
+                                },
+                                fit: BoxFit.cover,
+                              );
+                            },
+                          );
+                        } else if (state is LoggingOut) {
+                          return const DefaultCircleAvatar();
+                        } else if (state is LoginLoading) {
+                          return const ClipOval(
+                            child: SizedBox(
+                              width: 70,
+                              height: 70,
+                              child: ThemeShimmer(),
+                            ),
+                          );
+                        } else {
+                          return const DefaultCircleAvatar();
+                        }
+                      },
+                    ),
                   ),
                   BlocBuilder<LoginBloc, LoginState>(
                     builder: (context, state) {
@@ -103,9 +110,8 @@ class UserSection extends StatelessWidget {
                                   "Hi, ${state.jwt!['decoded_jwt']['name']}!",
                                   maxLines: 2,
                                   style: TextStyle(
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .onPrimary,
+                                    color:
+                                        Theme.of(context).colorScheme.onPrimary,
                                     fontSize: 18,
                                     fontWeight: FontWeight.bold,
                                   ),
