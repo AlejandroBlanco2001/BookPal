eb83dfac50e716e9bd86d43d43c28d68618746e1
Camilo Cespedes <cacespedes@uninorte.edu.co>
2023-11-24 09:56:29 -0500

    Nfc bloc as it should be

--- lib/presentation/nfc/bloc/nfc_bloc.dart
+++ lib/presentation/nfc/bloc/nfc_bloc.dart
@@ -19,17 +19,16 @@ class NfcBloc extends Bloc<NfcEvent, NfcState> {
   FutureOr<void> onScanNFC(NfcEvent event, Emitter<NfcState> emit) async {
     emit(NfcScanning());
     logger.d("Scanning NFC");
-    await Future.delayed(const Duration(seconds: 3), () => emit(const NfcScanned("1234567890")));
-    // try {
-    //   final nfcResult = await _readNfc();
-    //   logger.d("Result: $nfcResult");
-    //   if (nfcResult == '' || nfcResult.isEmpty) {
-    //     throw Exception('Empty response from nfc adapter');
-    //   }
-    //   emit(NfcScanned(nfcResult.trim()));
-    // } catch (e) {
-    //   logger.d("Error: $e");
-    //   emit(NfcError(error: Exception(e)));
-    // }
+    try {
+      final nfcResult = await _readNfc();
+      logger.d("Result: $nfcResult");
+      if (nfcResult == '' || nfcResult.isEmpty) {
+        throw Exception('Empty response from nfc adapter');
+      }
+      emit(NfcScanned(nfcResult.trim()));
+    } catch (e) {
+      logger.d("Error: $e");
+      emit(NfcError(error: Exception(e)));
+    }
   }
 }
